b4a8ec34f4eb6ab44675b932b2140c798d374d8f
gabriellaaaaaaa <132076667+gabriellaaaaaaa@users.noreply.github.com>
2024-04-02 11:35:13 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorSelectorAll(".botoes");
 
-for(let i=0; <botoes.length;i++>){
+for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
-        for(let j=0;j<botoes.length;j++>{
+        for(let j=0;j<botoes.length;j++>){
             botoes[j].classList.remove("ativo");
         }
 
